aa9454c6e9c1e85117965b5f14c1a2a329028e51
Jarrison Cano <jarrison.cano@hotmail.com>
2021-05-29 12:12:05 -0500

    Issue:#1. Refactored header

--- components/Home/Header.jsx
+++ components/Home/Header.jsx
@@ -1,44 +1,36 @@
-import styles from '../../styles/Home/components/Header.module.scss'
+import styled from '../../styles/Home/components/Header.module.scss'
 import Link from 'next/link'
 import { useEffect, useRef } from 'react'
 
 const Header = (props) => {
-  let link1 = useRef(null),
-    link2 = useRef(null),
-    link3 = useRef(null),
-    link = ''
+	let link1 = useRef(null),
+		link2 = useRef(null),
+		link3 = useRef(null),
+		link = ''
 
-  useEffect(() => {
-    if (props.id === 'home') link = link1.current
-    else if (props.id === 'proyects') link = link2.current
-    else if (props.id === 'abilities') link = link3.current
+	useEffect(() => {
+		if (props.id === 'home') link = link1.current
+		else if (props.id === 'proyects') link = link2.current
+		else if (props.id === 'abilities') link = link3.current
 
-    link.classList.add('headerActive')
-  }, [])
+		link.classList.add('headerActive')
+	}, [])
 
-  return (
-    <div className={styles.Header}>
-      <nav className={styles.nav}>
-        <ul className={styles.ul}>
-          <Link href='/'>
-            <li ref={link1} className={styles.li}>
-              INICIO
-            </li>
-          </Link>
-          <Link href='/proyectos'>
-            <li ref={link2} className={styles.li}>
-              PROYECTOS
-            </li>
-          </Link>
-          <Link href='/habilidades'>
-            <li ref={link3} className={styles.li}>
-              HABILIDADES
-            </li>
-          </Link>
-        </ul>
-      </nav>
-    </div>
-  )
+	return (
+		<div className={styled.Header}>
+			<ul>
+				<Link href='/'>
+					<li ref={link1}>INICIO</li>
+				</Link>
+				<Link href='/proyectos'>
+					<li ref={link2}>PROYECTOS</li>
+				</Link>
+				<Link href='/habilidades'>
+					<li ref={link3}>HABILIDADES</li>
+				</Link>
+			</ul>
+		</div>
+	)
 }
 
 export default Header
