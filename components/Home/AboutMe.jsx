fb6efefb459ea41765757223e67019227ca2968f
Jarrison Cano <jarrison.cano@hotmail.com>
2021-06-04 15:58:21 -0500

    Issue:#1. Added About layout and styles

--- components/Home/AboutMe.jsx
+++ components/Home/AboutMe.jsx
@@ -1,18 +1,27 @@
-import styles from '../../styles/Home/components/AboutMe.module.scss'
+import styled from '../../styles/Home/components/AboutMe.module.scss'
 
 const AboutMe = () => {
-  return (
-    <div className={styles.AboutMe}>
-      <h1>Sobre Mi</h1>
-      <p>
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Excepturi iusto
-        harum odit porro reprehenderit. Fugiat, deleniti quia nam rerum
-        recusandae, exercitationem molestias voluptate temporibus laboriosam
-        similique commodi reprehenderit, nostrum magnam qui itaque iste dicta
-        quasi laudantium fugit cum. Voluptatibus, tempore?
-      </p>
-    </div>
-  )
+	return (
+		<div className={styled.AboutMe}>
+			<h2> SOBRE MÍ</h2>
+			<div>
+				<p>
+					Actualmente vivo en Medellín, Colombia, dónde estudio la tecnología en
+					diseño y desarrollo web en el Tecnológico de Antioquia (TdeA), mi
+					hobby siempre ha sido la tecnología, lo que me llevó a aprender
+					programación desde una edad muy temprana.
+				</p>
+				<svg xmlns='http://www.w3.org/2000/svg'>
+					<rect fill='#FFE975' />
+				</svg>
+				<p>
+					Soy un amante del desarrollo con JavaScript, mi Stack de desarrollo
+					favorito es MERN (Mongo, Express, React Js, NodeJS). De igual forma,
+					me gusta experimentar nuevas tecnologías como C++, Kotlin o Phyton.
+				</p>
+			</div>
+		</div>
+	)
 }
 
 export default AboutMe
