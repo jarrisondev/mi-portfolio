46330589472da5ec8b6de78f61965bca029c8056
Jarrison Cano <jarrison.cano@hotmail.com>
2021-06-05 10:56:19 -0500

    Issue:#1. Created Footer component

---
+++ components/Footer/Footer.jsx
@@ -0,0 +1,14 @@
+import styled from '../../styles/Footer/Footer.module.scss'
+
+const Footer = () => {
+	return (
+		<footer className={styled.footer}>
+			<p>2021- Todos los derechos reservados </p>
+			<p>
+				Hecho con <span>&lt;3</span> por Jarrison Cano
+			</p>
+		</footer>
+	)
+}
+
+export default Footer
--- pages/index.jsx
+++ pages/index.jsx
@@ -2,17 +2,19 @@ import styles from '../styles/Home/Home.module.scss'
 import Header from '../components/Home/Header'
 import Content from '../components/Home/Content'
 import AboutMe from '../components/Home/AboutMe'
+import Footer from '../components/Footer/Footer'
 
 const index = () => {
-  return (
-    <div className={styles.HomeContainer}>
-      <div className={styles.HomeContent}>
-        <Header id='home' />
-        <Content />
-        <AboutMe />
-      </div>
-    </div>
-  )
+	return (
+		<div className={styles.HomeContainer}>
+			<div className={styles.HomeContent}>
+				<Header id='home' />
+				<Content />
+				<AboutMe />
+				<Footer />
+			</div>
+		</div>
+	)
 }
 
 export default index
