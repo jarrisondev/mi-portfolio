b616a056a051e14f4149689c3bc3a9a26936c346
Jarrison Cano <jarrison.cano@hotmail.com>
2021-06-08 22:43:18 -0500

    Issue:#3. Created and implemented context for the user preference language.

--- components/Layout/Layout.jsx
+++ components/Layout/Layout.jsx
@@ -1,7 +1,15 @@
+import { useState } from 'react'
 import styled from '../../styles/Layout/Layout.module.scss'
+import languageContext from '../../context/languageContext'
 
 const Layout = ({ children }) => {
-	return <div className={styled.Layout}>{children}</div>
+	const [language, setLanguage] = useState(false)
+
+	return (
+		<languageContext.Provider value={{ language, setLanguage }}>
+			<div className={styled.Layout}>{children}</div>
+		</languageContext.Provider>
+	)
 }
 
 export default Layout
---
+++ context/languageContext.js
@@ -0,0 +1,5 @@
+import { createContext } from 'react'
+
+const languageContext = createContext({})
+
+export default languageContext
