37a834502ac5d250045c01e8cf57b3403930cae3
Jarrison Cano <jarrison.cano@hotmail.com>
2021-06-09 20:54:09 -0500

    Issue:#2. Improve Projects component

--- src/components/Projects/Projects.jsx
+++ src/components/Projects/Projects.jsx
@@ -1,12 +1,14 @@
 import Cards from './Cards'
+import styled from '../../styles/Projects/Cards.module.scss'
 
 const Projects = (props) => {
 	return (
-		<>
+		<section className={styled.Proyects}>
+			<h2>{props.language.text.title}</h2>
 			{props.language.cards.map((projectsList, i) => (
 				<Cards key={i} projectsList={projectsList} text={props.language.text} />
 			))}
-		</>
+		</section>
 	)
 }
 
